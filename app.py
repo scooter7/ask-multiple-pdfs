42a79e2fa8565291b81fce3f2bf6c33bb70c028a
scooter7 <scooter.vineburgh@gmail.com>
2024-04-15 12:08:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("Chat with multiple PDFs :books:")
+    st.header("Proposal Exploration Tool :books:")
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
