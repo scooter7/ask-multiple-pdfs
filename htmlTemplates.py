72da4340df10b0ed1c4a93394d9338b20a340225
scooter7 <scooter.vineburgh@gmail.com>
2024-04-24 13:33:37 -0500

    Update htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -4,7 +4,7 @@ css = '''
     padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
 }
 .chat-message.user {
-    background-color: #2b313e
+    background-color: #002633
 }
 .chat-message.bot {
     background-color: #475063
