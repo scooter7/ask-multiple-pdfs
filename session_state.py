9ca5eab82a7c692b50b2c12bfbb0c90273511104
scooter7 <scooter.vineburgh@gmail.com>
2024-08-05 17:38:07 -0500

    Update session_state.py

--- session_state.py
+++ session_state.py
@@ -1,10 +1,4 @@
-try:
-    import streamlit.ReportThread as ReportThread
-    from streamlit.server.Server import Server
-except Exception:
-    # Streamlit >= 0.65.0
-    import streamlit.report_thread as ReportThread
-    from streamlit.server.server import Server
+import streamlit as st
 
 class SessionState:
     def __init__(self, **kwargs):
@@ -12,16 +6,7 @@ class SessionState:
             setattr(self, key, val)
 
 def get(**kwargs):
-    ctx = ReportThread.get_report_ctx()
-    current_server = Server.get_current()
-    session_infos = current_server._session_info_by_id.values()
-    
-    for session_info in session_infos:
-        s = session_info.session
-        if s == ctx.session:
-            this_session = s
-    
-    if not hasattr(this_session, '_custom_session_state'):
-        this_session._custom_session_state = SessionState(**kwargs)
-    
-    return this_session._custom_session_state
+    session = st.session_state
+    if not hasattr(session, '_custom_session_state'):
+        session._custom_session_state = SessionState(**kwargs)
+    return session._custom_session_state
