1936d0f3f11f49629744ccd8cf2dac813a517540
scooter7 <scooter.vineburgh@gmail.com>
2024-08-11 10:50:10 -0500

    Update authcai.py

--- authcai.py
+++ authcai.py
@@ -11,6 +11,7 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from langchain.schema import Document  # <-- Import Document here
 from htmlTemplates import css, bot_template, user_template
 from datetime import datetime
 import base64
